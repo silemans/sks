392a9c4234dbdc0901718bd07a9d21b73bf17dc8
silemans <128105956+silemans@users.noreply.github.com>
2024-06-26 13:23:56 +0530

    Update preparation.py

--- python preparation/preparation.py
+++ python preparation/preparation.py
@@ -2,7 +2,7 @@
 
 def lesser(a,b):
     if a%2==0 and b%2==0:
-        print(min(a,b))
-    elif (a%2==0 and b%2!=0) or (a%2!=0 and b%2==0):
-        print(max(a,b))
- 
+         result = min(a,b)
+    else:
+        result = max(a,b)
+    print(result)
